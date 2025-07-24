1c9322ffe6e8f427fdbbd2ca40f72151f90329ea
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-07-24 22:07:41 +0100

    FIXED arithmetic_operations AGAIN

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
-def perform_operation(num1, num2, operation) :
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide" :
-            if num2 != 0 :
-                return num1 / num2
-            else:
-                return "Cannot divide by 0"
-        case _ :
-            return "Incorrect input"
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cannot divide by 0"
+    else:
+        return "Incorrect input"
